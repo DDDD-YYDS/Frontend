ec90c14e074a276c3cdc108287ad3b2e45692f0c
Andy <andy910812@gmail.com>
2023-11-20 21:47:22 +0800

    chore: tailwindcss setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import eslint from 'vite-plugin-eslint'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
   devtools: { enabled: true },
   typescript: {
     typeCheck: true,
