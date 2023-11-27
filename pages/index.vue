3155d833adb3e54cf40c8b608c772b27a56719bd
Andy <andy910812@gmail.com>
2023-11-28 00:27:20 +0800

    feat: header UI

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header class="flex justify-between bg-[#8F6A6A] h-20 items-center">
+      <h2 class="mx-auto text-4xl text-black">留言板</h2>
+      <nav class="pr-3">
+        <a href="#" class="px-3 text-2xl">首頁</a>
+        <button class="px-3 text-2xl">登入</button>
+        <button class="px-3 text-2xl">註冊</button>
+      </nav>
+    </header>
+  </div>
+</template>
+<style>
+* {
+  font-family: Inter;
+}
+</style>
